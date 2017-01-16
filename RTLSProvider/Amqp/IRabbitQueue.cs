e576a41c346b1e6f085586a5c4eee4cbc91a8807
Reza Alemy <reza@alemy.net>
2017-01-15 20:49:55 -0800

    RabbitMQ Controller and Interface added

--- RTLSProvider/Amqp/IRabbitQueue.cs
+++ RTLSProvider/Amqp/IRabbitQueue.cs
@@ -15,15 +15,20 @@ namespace RTLSProvider.Amqp
         void Initialize(ConnectionFactory factory);
         void AddReceiver(EventHandler<BasicDeliverEventArgs> receiver);
         void Consume(String queue);
+        void ReleaseQueue();
     }
 
     public class QueueFactoryParams
     {
-        public String HostName;
-        public Int32 Port;
-        public String VirtualHost;
+        public String HostName = "localhost";
+        public Int32 Port = 5672;
+        public String VirtualHost = "/";
         public String UserName;
         public String Password;
-        public Boolean AutomaticRecoveryEnabled;
+        public Boolean AutomaticRecoveryEnabled = true;
+        public Boolean QueueNoAck = true;
+        public Boolean QueueDurable = false;
+        public Boolean QueueExclusive = false;
+        public Boolean AutoDelete = false;
     }
 }
---
+++ RTLSProvider/Amqp/RabbitQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RTLSProvider.Amqp
+{
+    public class RabbitQueue
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private EventingBasicConsumer _consumer;
+        private QueueFactoryParams _factoryParams;
+
+        public void Initialize(ConnectionFactory connectionFactory)
+        {
+            if (connectionFactory != null) _connection = connectionFactory.CreateConnection();
+            if (_connection != null) _channel = _connection.CreateModel();
+            if (_channel != null) _consumer = new EventingBasicConsumer(_channel);
+        }
+
+        public ConnectionFactory CreateFactory(QueueFactoryParams factoryParams)
+        {
+            _factoryParams = factoryParams;
+            return new ConnectionFactory()
+            {
+                HostName = factoryParams.HostName,
+                Port = factoryParams.Port,
+                VirtualHost = factoryParams.VirtualHost,
+                UserName = factoryParams.UserName,
+                Password = factoryParams.Password,
+                AutomaticRecoveryEnabled = factoryParams.AutomaticRecoveryEnabled
+            };
+        }
+
+        public void AddReceiver(EventHandler<BasicDeliverEventArgs> action)
+        {
+            _consumer.Received += action;
+        }
+
+        public void Consume(string queue)
+        {
+            _channel.BasicConsume(queue: queue,
+                noAck: true,
+                consumer: _consumer);
+        }
+
+        public void Declare(string queue)
+        {
+            if (_factoryParams != null)
+                _channel.QueueDeclare(queue: queue, durable: _factoryParams.QueueDurable,
+                    exclusive: _factoryParams.QueueExclusive,
+                    autoDelete: _factoryParams.AutoDelete, arguments: null);
+        }
+
+        public void Publish(string queue, string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "", routingKey: queue, basicProperties: null, body: body);
+        }
+
+        public void ReleaseQueue()
+        {
+            if (_channel != null && _channel.IsOpen)
+                _channel.Close();
+            if (_connection != null && _connection.IsOpen)
+                _connection.Close();
+        }
+    }
+}
